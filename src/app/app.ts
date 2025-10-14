7278f878c9c83eafedec764131bfd7ff49eb06a1
GaboTNSINF <gabrielhernan1261@gmail.com>
2025-10-14 12:59:44 -0300

    Archivo 2 completo

---
+++ src/app/app.ts
@@ -0,0 +1,38 @@
+import { Component, signal, computed } from '@angular/core';
+import { ContadorComponent } from './contador/contador';
+import { ContadorSignalsComponent } from './contador-signals/contador-signals';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ContadorComponent, ContadorSignalsComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = signal('¡Angular 20 con Signals!');
+  mensaje = signal('Mi primera aplicación Angular');
+  contador = signal(0);
+  totalClicks = signal(0);
+  
+    cambiarMensaje() {
+    this.mensaje.set(this.mensaje() === 'Mi primera aplicación Angular' 
+      ? '¡Angular 20 es genial!' 
+      : 'Mi primera aplicación Angular');
+    this.contador.update(c => c + 1);
+  }
+  // Computed signal para estadísticas
+  estadisticas = computed(() => {
+    const clicks = this.totalClicks();
+    if (clicks === 0) return 'Sin interacciones';
+    if (clicks < 10) return 'Explorando...';
+    if (clicks < 25) return 'Aprendiendo...';
+    if (clicks < 50) return 'Progresando...';
+    return '¡Dominando Angular!';
+  });
+  
+  onContadorCambio(nuevoValor: number) {
+    this.totalClicks.update(c => c + 1);
+    console.log('Contador cambió a:', nuevoValor);
+  }
+}
